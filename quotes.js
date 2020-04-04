a9b1854de314d55a56c2476bcdbd6e2a0e2ebafb
mgweaver <masongweaver@gmail.com>
2020-04-04 15:34:49 -0600

    add switch statement

--- quotes.js
+++ quotes.js
@@ -1,20 +1,34 @@
-const familyDiv = document.getElementById('familyDiv');
-const agencyDiv = document.getElementById('agencyDiv');
-const atonementDiv = document.getElementById('atonementDiv');
-const addictionDiv = document.getElementById('addictionDiv');
-const serviceDiv = document.getElementById('serviceDiv');
-const lazinessDiv = document.getElementById('lazinessDiv');
-const repentanceDiv = document.getElementById('repentanceDiv');
-const forgivenessDiv = document.getElementById('forgivenessDiv');
+const displayDiv = document.getElementById('categories');
+const displayTable = document.getElementById('displayTable');
 
-const familyTable = document.getElementById('familyTable');
-const agencyTable = document.getElementById('agencyTable');
-const atonementTable = document.getElementById('atonementTable');
-const addictionTable = document.getElementById('addictionTable');
-const serviceTable = document.getElementById('serviceTable');
-const lazinessTable = document.getElementById('lazinessTable');
-const repentanceTable = document.getElementById('repentanceTable');
-const forgivenessTable = document.getElementById('forgivenessTable');
+function showQuotes(topic) {
+  switch (topic) {
+    case "Family":
+      showFamily();
+      break;
+    case "Agency":
+      showAgency();
+      break;
+    case "Atonement":
+      showAtonement();
+      break;
+    case "Addiction":
+      showAddiction();
+      break;
+    case "Service":
+      showService();
+      break;
+    case "Laziness":
+      showLaziness();
+      break;
+    case "Repentance":
+      showRepentance();
+      break;
+    case "Forgiveness":
+      showForgiveness();
+      break;
+  }
+}
 
 function showFamily() {
   const request = new XMLHttpRequest();
@@ -22,9 +36,9 @@ function showFamily() {
   request.onload = () => {
     try {
       const json = JSON.parse(request.responseText);
-      populateTable(json, familyDiv, familyTable);
+      populateTable(json, displayDiv, displayTable);
     } catch (e) {
-      familyDiv.innerHTML = "Could not load quotes."
+      displayDiv.innerHTML = "Could not load quotes."
     }
   };
   request.send();
@@ -36,9 +50,9 @@ function showAgency() {
   request.onload = () => {
     try {
       const json = JSON.parse(request.responseText);
-      populateTable(json, agencyDiv, agencyTable);
+      populateTable(json, displayDiv, displayTable);
     } catch (e) {
-      agencyDiv.innerHTML = "Could not load quotes."
+      displayDiv.innerHTML = "Could not load quotes."
     }
   };
   request.send();
@@ -50,9 +64,9 @@ function showAtonement() {
   request.onload = () => {
     try {
       const json = JSON.parse(request.responseText);
-      populateTable(json, atonementDiv, atonementTable);
+      populateTable(json, displayDiv, displayTable);
     } catch (e) {
-      atonementDiv.innerHTML = "Could not load quotes."
+      displayDiv.innerHTML = "Could not load quotes."
     }
   };
   request.send();
@@ -64,9 +78,9 @@ function showAddiction() {
   request.onload = () => {
     try {
       const json = JSON.parse(request.responseText);
-      populateTable(json, addictionDiv, addictionTable);
+      populateTable(json, displayDiv, displayTable);
     } catch (e) {
-      addictionDiv.innerHTML = "Could not load quotes."
+      displayDiv.innerHTML = "Could not load quotes."
     }
   };
   request.send();
@@ -78,9 +92,9 @@ function showService() {
   request.onload = () => {
     try {
       const json = JSON.parse(request.responseText);
-      populateTable(json, serviceDiv, serviceTable);
+      populateTable(json, displayDiv, displayTable);
     } catch (e) {
-      serviceDiv.innerHTML = "Could not load quotes."
+      displayDiv.innerHTML = "Could not load quotes."
     }
   };
   request.send();
@@ -92,9 +106,9 @@ function showLaziness() {
   request.onload = () => {
     try {
       const json = JSON.parse(request.responseText);
-      populateTable(json, lazinessDiv, lazinessTable);
+      populateTable(json, displayDiv, displayTable);
     } catch (e) {
-      lazinessDiv.innerHTML = "Could not load quotes."
+      displayDiv.innerHTML = "Could not load quotes."
     }
   };
   request.send();
@@ -106,9 +120,9 @@ function showRepentance() {
   request.onload = () => {
     try {
       const json = JSON.parse(request.responseText);
-      populateTable(json, repentanceDiv, repentanceTable);
+      populateTable(json, displayDiv, displayTable);
     } catch (e) {
-      repentanceDiv.innerHTML = "Could not load quotes."
+      displayDiv.innerHTML = "Could not load quotes."
     }
   };
   request.send();
@@ -120,9 +134,9 @@ function showForgiveness() {
   request.onload = () => {
     try {
       const json = JSON.parse(request.responseText);
-      populateTable(json, forgivenessDiv, forgivenessTable);
+      populateTable(json, displayDiv, displayTable);
     } catch (e) {
-      forgivenessDiv.innerHTML = "Could not load quotes."
+      displayDiv.innerHTML = "Could not load quotes."
     }
   };
   request.send();
